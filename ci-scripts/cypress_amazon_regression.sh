5e71703517f5dc3dd88849efccf501d388a58049
premsai <premsai.tippaluri@zemosolabs.com>
2022-09-13 15:44:04 +0530

    Check for webhook

--- ci-scripts/cypress_amazon_regression.sh
+++ ci-scripts/cypress_amazon_regression.sh
@@ -1,7 +1,7 @@
 echo "Running regression tests for Amazon site"
 
 
-echo "Run  jenkins job  using the webhooks"
+echo "Run  jenkins  job  using the webhooks"
 npx cypress run --spec 'cypress/integration/2-advanced-examples/homepage.spec.ts'
 
 
