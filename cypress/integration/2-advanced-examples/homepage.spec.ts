0b15533c2b53f7dda749fb8fc356761d5c814b04
premsai <premsai.tippaluri@zemosolabs.com>
2022-09-14 09:27:22 +0530

    Check for webhook

--- cypress/integration/2-advanced-examples/homepage.spec.ts
+++ cypress/integration/2-advanced-examples/homepage.spec.ts
@@ -1,9 +1,9 @@
 describe('Verify amazon shopping website home page', () => {
-    it('Verify visiting the url', () => {
-        cy.visit('https://www.amazon.in/')
-        cy.url().should('include','amazon')
-    })
-    it('Verify amazon sopping website home page',()=>{
-        cy.get('[aria-label="Amazon"]',{timeout:10000}).should('be.visible')
-    })
-})
+//     it('Verify visiting the url', () => {
+//         cy.visit('https://www.amazon.in/')
+//         cy.url().should('include','amazon')
+//     })
+//     it('Verify amazon sopping website home page',()=>{
+//         cy.get('[aria-label="Amazon"]',{timeout:10000}).should('be.visible')
+//     })
+// })
